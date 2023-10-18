541e18973c91db36744064b7c656c877d579ba60
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-10-18 12:00:40 +0300

    Create MaximumCoins.java

---
+++ MaximumCoins.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int left=0;
+        int right=piles.length-2;
+        int sum=0;
+        while(left<right){
+            sum+=piles[right];
+            right-=2;
+            left+=1;
+        }
+        return sum;
+    }
+}
