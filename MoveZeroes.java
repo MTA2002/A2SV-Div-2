fed3051fccf247e0fc32980a0e8fe113bdab0d48
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-10-19 23:13:42 +0300

    Create MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int j=0;
+        int temp=0;
+        while(j<nums.length){
+            if(nums[j]!=0){
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+            j++;
+        }
+    }
+    
+}
+       
