ac630c65f4a4dddb83d4b527cd1e27bba2be235f
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-10-26 23:19:02 +0300

    Create Permutation in String.java

---
+++ Permutation in String.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkInclusion(String p, String s) {
+        HashMap<Character,Integer> sMap=new HashMap<>();
+        HashMap<Character,Integer> pMap=new HashMap<>();
+
+        for(int i=0;i<p.length();i++){
+            if(pMap.get(p.charAt(i))!=null){
+                pMap.put(p.charAt(i),pMap.get(p.charAt(i))+1);
+            }else{
+                pMap.put(p.charAt(i),1);
+            }
+        }
+        for(int i=0;i<s.length();i++){
+            if(sMap.get(s.charAt(i))!=null){
+                sMap.put(s.charAt(i),sMap.get(s.charAt(i))+1);
+            }else{
+                sMap.put(s.charAt(i),1);
+            }
+            if(i>=(p.length()-1)){                 
+                if(sMap.equals(pMap)){
+                    return true;
+                }
+                char leftChar = s.charAt(i-(p.length()-1));
+                sMap.put(leftChar, sMap.get(leftChar) - 1);
+                if (sMap.get(leftChar) == 0) {
+                    sMap.remove(leftChar);
+                }
+            }
+        }
+        return false;
+    }
+}
